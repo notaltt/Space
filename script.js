e348a085caf13873537e9d46673627158d4a1ebd
notaltt <cabanerojeb@gmail.com>
2023-06-02 19:49:12 +0800

    new commit

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const menuToggle = document.querySelector('.toggle');
 const showcase = document.querySelector('.showcase');
+const about = document.querySelector('.about');
+const contact = document.querySelector('.contact');
 
 menuToggle.addEventListener('click', () => {
-    menuToggle.classList.toggle('active')
+    menuToggle.classList.toggle('active');
     showcase.classList.toggle('active');
+    about.classList.toggle('active');
+    contact.classList.toggle('active');
 })
+
+let lastTime;
+function update(time){
+    if(lastTime == null){
+        lastTime = time;
+        window.requestAnimationFrame(update);
+        return;
+    }
+
+    const delta = time - lastTime;
+    lastTime = time;
+}
 
 exports.build = series(scssTask, jsTask);
